45b028863e655fc839e9fd12d7b00abda3b6332c
kingcryptodev <kingcryptodev@gmx.com>
2014-11-08 13:06:56 -0500

    Update version.h

--- src/version.h
+++ src/version.h
@@ -14,8 +14,8 @@
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   00000 * CLIENT_VERSION_MINOR
-                         +     200 * CLIENT_VERSION_REVISION
-                         +       3 * CLIENT_VERSION_BUILD;
+                         +     100 * CLIENT_VERSION_REVISION
+                         +       1 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
